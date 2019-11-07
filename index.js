d7180b88abf5f996ec3df264bb3ebdd6c08a9122
kat-star <katherinewkwong@gmail.com>
2019-11-06 16:59:39 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+
+const random = Math.round(Math.random() * 10)
+
+const mod = 100 % 24
+
+const max = Math.max(20, 1, 4)
